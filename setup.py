09678437c21aa03f288ea181734b0a8bd4a7727b
hafizharis246 <harismehmood948@gmail.com>
2025-02-22 03:18:19 +0500

    new files

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path: str) -> List[str]:
     """Returns a list of requirements from a requirements file."""
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
 setup(
     name='mlproject',
     version='0.0.1',
-    author= "Haris",
-    author_email= "harismehmood948@gmail.com",
+    author="Haris",
+    author_email="harismehmood948@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
